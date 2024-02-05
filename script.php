e1fccf354661fb4d635a5526445c5629cbac8b1b
Soniamy <marianna.marzullo@gmail.com>
2024-02-05 15:03:11 +0100

    Sostituzione della parola scritta con gli asterischi nel paragrafo e stampo la sua nuova lunghezza

--- script.php
+++ script.php
@@ -6,5 +6,8 @@ echo var_dump($parola, $paragrafo);
 //Stampa del paragrafo e la sua lunghezza
 echo $paragrafo . "<br>";
 echo "Il paragrafo è lungo " . strlen($paragrafo) . " caratteri" . "<br>";
+// Sostituisco la parola scritta con gli asterischi nel paragrafo e lo stampo  con la sua lunghezza
+echo str_replace($parola, '***', $paragrafo) . "<br>";
+echo  "Il paragrafo censurato è lungo " . strlen( str_replace($parola, '***', $paragrafo)) . " caratteri";
 
 ?>
