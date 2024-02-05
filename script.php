84ca87a914324f317f4819f1c3d406cf9b8c93ee
Soniamy <marianna.marzullo@gmail.com>
2024-02-05 14:40:22 +0100

    Creazione del form per inserire le parole e il paragrafo, e creazione del componente script.php per inserire le variabili $parole e $paragrafo di php

--- script.php
+++ script.php
@@ -3,15 +3,5 @@
 $parola = $_GET["parola"];
 $paragrafo = $_GET["paragrafo"];
 
-/* echo var_dump($parola, $paragrafo); */
-// Stampo a schermo il paragrafo e la sua lunghezza
-echo $paragrafo . "<br>";
-echo "Il paragrafo è lungo " . strlen($paragrafo) . " caratteri" . "<br>";
-
-// Sostituisco la parola scritta con gli asterischi nel paragrafo e lo stampo a schermo con la sua lunghezza
-// Rendo la parola minuscola per censurare anche le parole maiuscole
-$paragrafoCensurato = str_replace(strtolower($parola), '***', $paragrafo) . "<br>";
-echo $paragrafoCensurato;
-echo "Il paragrafo censurato è lungo " . strlen($paragrafo) . " caratteri";
 
 ?>
